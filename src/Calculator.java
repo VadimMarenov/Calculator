5c639141a5e5cfacfd2a28f4ae1b9b7acb13d97a
Vadim <v.marenov@bk.ru>
2022-03-21 17:41:15 +0300

    Change devide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,7 +10,15 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y != 0) {
+            return x / y;
+        } else {
+            System.out.println("DIVIDING BY ZERO");
+            return Integer.MAX_VALUE;
+
+        }
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
